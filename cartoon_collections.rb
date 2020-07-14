622b92ad9dd0bb550627e3e96779ca70a4446053
Morgan Matney <morgan.matney89@gmail.com>
2020-07-14 21:05:32 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,14 +10,10 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? {|call| call.length > 4} ? true:false
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese(string)
-  i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
-  while i < cheese_types.length
-    return cheese_types[i] if string.include?(cheese_types[i])
-    i += 1
-  end
+  string.find {|food| cheese_types.include?(food)}
 end
